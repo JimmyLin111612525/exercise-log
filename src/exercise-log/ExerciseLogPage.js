f8faa740dc3b3c5f53e071a1e90fe7836a9a4642
Jimmy Lin <jimmy.lin@stonybrook.edu>
2021-01-27 17:27:26 -0500

    Added a datepicker that changes to log of another day

--- src/exercise-log/ExerciseLogPage.js
+++ src/exercise-log/ExerciseLogPage.js
@@ -1,4 +1,4 @@
-import React, {Component, useContext,useEffect,useState} from 'react'
+import React, {useContext,useEffect,useState} from 'react'
 import { auth, firestore } from '../Firestore'
 import { UserContext } from '../providers/UserProvider'
 
@@ -8,16 +8,20 @@ const ExerciseLogPage=()=>{
 
 
     const user=useContext(UserContext)
-    const {displayName, email}=user
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
+    //const {displayName, email}=user
+    
 
-    let cur_date = mm + '/' + dd + '/' + yyyy;
+    
 
     useEffect(()=>{
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        let cur_date = mm + '/' + dd + '/' + yyyy;
+
         let docs=firestore.collection('logs').where('date','==', cur_date).where('uid','==',user.uid)
+        setDate(`${yyyy}-${mm}-${dd}`)
         let empty=false
         let doc_found
         docs.get().then((querySnapshot)=>{
@@ -40,8 +44,16 @@ const ExerciseLogPage=()=>{
         setText(e.target.value)
     }
 
+    const upDateDate=(e)=>{
+        setDate(e.target.value)
+        console.log(e.target.value)
+    }
+
     const saveLog=(e,date,text)=>{
-        let docs=firestore.collection('logs').where('date','==', cur_date).where('uid','==',user.uid)
+        let splitted=date.split("-")
+        let new_date=`${splitted[1]}/${splitted[2]}/${splitted[0]}`
+        console.log(new_date)
+        let docs=firestore.collection('logs').where('date','==', new_date).where('uid','==',user.uid)
         let empty=false
         let doc_found
         docs.get().then((querySnapshot)=>{
@@ -50,21 +62,25 @@ const ExerciseLogPage=()=>{
             })
             empty=querySnapshot.empty
         }).then(()=>{
-            if(empty==true){
+            if(empty){
                 console.log("make new document")
-                firestore.collection('logs').doc().set({date:cur_date,text:text,uid:user.uid})
+                firestore.collection('logs').doc().set({date:new_date,text:text,uid:user.uid})
             }
             else{
                 console.log("update document")
                 firestore.collection('logs').doc(doc_found.id).update({text:text})
             }
         })
-        console.log(text,", ",cur_date)
+        console.log(text,", ",new_date)
     }
 
         return (
             <div>
-                <h1>Log for {cur_date}</h1>
+                <h1>Log for {date}</h1>
+                <label>Change date: {" "}</label>
+                <input type="date" value={date} onChange={(e)=>upDateDate(e)}/>
+                <br></br>
+                <br></br>
                 <label>Text:{" "}</label>
                 <input type="text" value={text} onChange={(e)=>upDateText(e)}/>{" "}
                 <button onClick={(e)=>saveLog(e, date,text)}>Save</button>
