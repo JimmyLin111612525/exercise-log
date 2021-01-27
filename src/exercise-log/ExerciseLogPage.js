b02f0b7601e444c4ea4e2a1ec1549a98877a777f
Jimmy Lin <jimmy.lin@stonybrook.edu>
2021-01-27 16:56:11 -0500

    Added a temporary text box for the log as a test

--- src/exercise-log/ExerciseLogPage.js
+++ src/exercise-log/ExerciseLogPage.js
@@ -1,17 +1,73 @@
-import React, {Component, useContext} from 'react'
-import { auth } from '../Firestore'
+import React, {Component, useContext,useEffect,useState} from 'react'
+import { auth, firestore } from '../Firestore'
 import { UserContext } from '../providers/UserProvider'
 
 const ExerciseLogPage=()=>{
+    const[text,setText]=useState('')
+    const[date,setDate]=useState('')
+
+
     const user=useContext(UserContext)
     const {displayName, email}=user
-    console.log(user)
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    let cur_date = mm + '/' + dd + '/' + yyyy;
+
+    useEffect(()=>{
+        let docs=firestore.collection('logs').where('date','==', cur_date).where('uid','==',user.uid)
+        let empty=false
+        let doc_found
+        docs.get().then((querySnapshot)=>{
+            querySnapshot.forEach((doc)=>{
+                doc_found=doc
+            })
+            empty=querySnapshot.empty
+        }).then(()=>{
+            if (empty){
+                setText('')
+            }else{
+                firestore.collection('logs').doc(doc_found.id).get().then((d)=>{
+                    setText(d.data().text)
+                })
+            }
+        })
+    },[])
+
+    const upDateText=(e)=>{
+        setText(e.target.value)
+    }
+
+    const saveLog=(e,date,text)=>{
+        let docs=firestore.collection('logs').where('date','==', cur_date).where('uid','==',user.uid)
+        let empty=false
+        let doc_found
+        docs.get().then((querySnapshot)=>{
+            querySnapshot.forEach((doc)=>{
+                doc_found=doc
+            })
+            empty=querySnapshot.empty
+        }).then(()=>{
+            if(empty==true){
+                console.log("make new document")
+                firestore.collection('logs').doc().set({date:cur_date,text:text,uid:user.uid})
+            }
+            else{
+                console.log("update document")
+                firestore.collection('logs').doc(doc_found.id).update({text:text})
+            }
+        })
+        console.log(text,", ",cur_date)
+    }
+
         return (
             <div>
-                <h1>Hello!</h1>
-                <h2>{displayName}</h2>
-                <h3>{email}</h3>
-                
+                <h1>Log for {cur_date}</h1>
+                <label>Text:{" "}</label>
+                <input type="text" value={text} onChange={(e)=>upDateText(e)}/>{" "}
+                <button onClick={(e)=>saveLog(e, date,text)}>Save</button>
             </div>
         )
     
